9228e9107a0e2bc7a80e2fe2efb328ce5b74281c
Tom Mo <tom217@foxmail.com>
2022-02-04 18:50:40 -0800

    add tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,57 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es6": true,
-		"node": true
+	env: {
+		browser: true,
+		es6: true,
+		node: true
 	},
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"project": "tsconfig.json",
-		"sourceType": "module"
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		// project: './tsconfig.json',
+		sourceType: "module"
 	},
-	"plugins": [
+	plugins: [
 		"@typescript-eslint",
 		"jsdoc",
 		"import",
 	],
-	"extends": [
+	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:import/recommended",
 		"plugin:import/typescript"
 	],
-	"overrides": [
+	overrides: [
 		{
-			"files": ["**/*.spec.ts"],
-			"rules": {
+			files: ["**/*.spec.ts"],
+			rules: {
 				"max-lines": "off",
 				"max-lines-per-function": "off",
 				"no-unused-expressions": "off"
 			}
 		}
 	],
-	"rules": {
-		"@typescript-eslint/array-type": ["error", {"default": "array-simple"}],
+	rules: {
+		"@typescript-eslint/array-type": ["error", {default: "array-simple"}],
 		"@typescript-eslint/ban-types": [
 			"error",
 			{
-				"types": {
-					"Object": "Avoid using the `Object` type. Did you mean `object`?",
-					"Function": "Avoid using the `Function` type. Prefer a specific function type, like `() => void`.",
-					"Boolean": "Avoid using the `Boolean` type. Did you mean `boolean`?",
-					"Number": "Avoid using the `Number` type. Did you mean `number`?",
-					"String": "Avoid using the `String` type. Did you mean `string`?",
-					"Symbol": "Avoid using the `Symbol` type. Did you mean `symbol`?",
+				types: {
+					Object: "Avoid using the `Object` type. Did you mean `object`?",
+					Function: "Avoid using the `Function` type. Prefer a specific function type, like `() => void`.",
+					Boolean: "Avoid using the `Boolean` type. Did you mean `boolean`?",
+					Number: "Avoid using the `Number` type. Did you mean `number`?",
+					String: "Avoid using the `String` type. Did you mean `string`?",
+					Symbol: "Avoid using the `Symbol` type. Did you mean `symbol`?",
 				}
 			}
 		],
-		"@typescript-eslint/explicit-member-accessibility": ["error", {"overrides": {"constructors": "off"}}],
+		"@typescript-eslint/explicit-member-accessibility": ["error", {overrides: {constructors: "off"}}],
 		"@typescript-eslint/explicit-module-boundary-types": "off",
 		"@typescript-eslint/consistent-type-assertions": "error",
 		"@typescript-eslint/naming-convention": [
 			"error",
-			{"selector": "typeLike", "format": ["PascalCase"]},
-			{"selector": "variable", "format": ['camelCase', "PascalCase", "UPPER_CASE"]}
+			{selector: "typeLike", format: ["PascalCase"]},
+			{selector: "variable", format: ["camelCase", "PascalCase", "UPPER_CASE"]}
 		],
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/no-inferrable-types": "off",
@@ -71,10 +71,10 @@ module.exports = {
 		"constructor-super": "error",
 		"curly": "error",
 		"eol-last": "error",
-		"eqeqeq": ["error", "always", {"null": "ignore"}],
-		"indent": ["error", "tab", {"ignoreComments": true, "SwitchCase": 1}],
-		"max-len": ["error", {"code": 120, "ignoreComments": true}],
-		"max-lines-per-function": ["error", {"max": 50, "skipComments": true}],
+		"eqeqeq": ["error", "always", {null: "ignore"}],
+		"indent": ["error", "tab", {ignoreComments: true, SwitchCase: 1}],
+		"max-len": ["error", {code: 120, ignoreComments: true}],
+		"max-lines-per-function": ["error", {max: 50, skipComments: true}],
 		"max-lines": ["error", 300],
 		"new-parens": "error",
 		"no-await-in-loop": "error",
@@ -97,12 +97,12 @@ module.exports = {
 		"no-var": "error",
 		"prefer-const": "off",
 		"quote-props": ["error", "consistent-as-needed"],
-		"quotes": ["error", "double", {"avoidEscape": true}],
+		"quotes": ["error", "double", {avoidEscape: true}],
 		"radix": "error",
 		"semi": "error",
-		"space-before-function-paren": ["error", {"anonymous": "ignore", "named": "ignore", "asyncArrow": "always"}],
+		"space-before-function-paren": ["error", {anonymous: "ignore", named: "ignore", asyncArrow: "always"}],
 		"space-infix-ops": "error",
-		"space-unary-ops": ["error", {"words": true, "nonwords": false}],
+		"space-unary-ops": ["error", {words: true, nonwords: false}],
 		"spaced-comment": "error",
 		"use-isnan": "error",
 		"jsdoc/check-alignment": 2,
