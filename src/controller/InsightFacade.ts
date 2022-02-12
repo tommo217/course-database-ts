ec84d969e364ddf5d4f7e70bc824cb18bf72f3c5
Tom Mo <tom217@foxmail.com>
2022-02-11 17:53:17 -0800

    query syntax check + query.bnf file

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -6,6 +6,7 @@ import {
 	InsightResult,
 	NotFoundError
 } from "./IInsightFacade";
+import {parseQuery, Query} from "./Query";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -26,18 +27,29 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public performQuery(query: unknown): Promise<InsightResult[]> {
-		console.log(typeof query);
-		let queryObj;
-		console.log(typeof query);
-		if (typeof query === "string") {
-			let queryStr = query as string;
-			queryObj = JSON.parse(queryStr);
-		} else {
-			queryObj = query;
-		}
+		return new Promise<InsightResult[]>( (resolve, reject) => {
+			try {
+				let queryObj;
+				// console.log("Type of query: ", typeof query);
+				if (typeof query === "string") {
+					let queryStr = query as string;
+					queryObj = JSON.parse(queryStr);
+				} else {
+					queryObj = query;
+				}
+				let q: Query = parseQuery(queryObj);
+				// console.log(q);
+				resolve([{test: "no syntax error"}]);
+			} catch (err) {
+				if (err instanceof Error) {
+					reject(new InsightError(err.message));
+				} else {
+					reject(new InsightError());
+				}
+			}
+		});
 
 
-		return Promise.resolve([{test:"test"}]);
 		// return Promise.reject("Not implemented.");
 	}
 
@@ -62,17 +74,60 @@ export default class InsightFacade implements IInsightFacade {
 /**
  * TODO: Delete/Comment out! test script for performQuery
  */
-let facade: InsightFacade = new InsightFacade();
-let query = {
-	WHERE: {
-		GT: {courses_avg: 97}
-	},
-	OPTIONS: {
-		COLUMNS: ["courses_dept","courses_avg"],
-		ORDER: "courses_avg"
-	}
-};
+// let facade: InsightFacade = new InsightFacade();
+// let query = {
+// 	WHERE: {
+// 		// GT: {courses_avg: 97}
+// 		IS:{courses_dept:"math"}
+// 	},
+// 	OPTIONS: {
+// 		COLUMNS: ["courses_dept","courses_avg"],
+// 		ORDER: "courses_avg"
+// 	}
+// };
+//
+// let queryC = {
+// 	WHERE: {
+// 		OR: [
+// 			{
+// 				AND: [
+// 					{
+// 						GT: {
+// 							courses_avg: 90
+// 						}
+// 					},
+// 					{
+// 						IS: {
+// 							courses_dept: "adhe"
+// 						}
+// 					}
+// 				]
+// 			},
+// 			{
+// 				EQ: {
+// 					courses_avg: 95
+// 				}
+// 			}
+// 		]
+// 	},
+// 	OPTIONS: {
+// 		COLUMNS: [
+// 			"courses_dept",
+// 			"courses_id",
+// 			"courses_avg"
+// 		],
+// 		ORDER: "courses_avg"
+// 	}
+// };
+//
+// let queryInvalid = {
+// 	WHERE: {
+// 		// GT: {courses_avg: 97}
+// 		IS:{courses_dept:"math"}
+// 	}
+// };
+//
+//
+// // facade.performQuery(JSON.stringify(query));
+// // let q: Query = parseQuery(queryInvalid);
 
-
-facade.performQuery(JSON.stringify(query));
-// facade.performQuery(query);
--- src/controller/Query.ts
+++ src/controller/Query.ts
@@ -1,12 +1,128 @@
-// Custom Query Class function
+/**
+ * Given the query JSON, return query struct with body & options
+ */
+function parseQuery(input: any): Query{
+	if (input.WHERE !== undefined && input.OPTIONS !== undefined) {
+		let inputBody = parseFilter(input.WHERE);
+		let inputOptions = new Options();
+		inputOptions.deserialize(input.OPTIONS);
 
+		return {
+			body: inputBody,
+			options: inputOptions
+		};
+	}
+
+	throw new Error("Syntax error: WHERE or OPTIONS missing");
+}
+
+
+/**
+ * Given a JSON object of query BODY, return custom filter class
+ * @param input: a JSON object without struct
+ */
+function parseFilter(input: any): Filter{
+	if (Object.keys(input).length === 1) {
+		for (let op in input) {
+			if (logicOps.includes(op)) {
+				let f = new LogicComparison();
+				if (f.deserialize(input) === 1) {
+					return f;
+				}
+			}
+			if (mComparators.includes(op)) {
+				let f = new MComparison();
+				if (f.deserialize(input) === 1) {
+					return f;
+				}
+			}
+			if (op === "IS") {
+				let f = new SComparison();
+				if (f.deserialize(input) === 1) {
+					return f;
+				}
+			}
+			if (op === "NOT") {
+				let f = new Negation();
+				if (f.deserialize(input) === 1) {
+					return f;
+				}
+			}
+		}
+	}
+
+	throw new Error("Parsing failed, syntax error");
+}
+
+/**
+ * Struct and class definitions
+ */
+interface Query {
+	body: Filter;
+	options: Options;
+}
+
+class Options {
+	public idString: string; // specifies database
+	public columns: string[];
+	public order: string;
+
+	constructor() {
+		this.idString = "";
+		this.columns = [];
+		this.order = "";
+	}
+
+	public deserialize(input: any) {
+		if (input.COLUMNS !== undefined) {
+			for (let i in input.COLUMNS) {
+				let [idStr, field] = parseKey(input.COLUMNS[i], 2);
+				this.columns.push(field);
+				if (this.idString === "") {
+					this.idString = idStr;
+				} else if (this.idString !== idStr) {
+					throw new Error("Syntax error: multiple db referenced: " + idStr);
+				}
+			}
+
+			if (input.ORDER !== undefined) {
+				this.order = input.ORDER;
+				return;
+			}
+		}
+		throw new Error("Syntax error: missing COLUMNS or ORDER");
+	}
+}
+
+/**
+ * parse mkey or skey as a tuple of idstring and fields
+ * @param keyType: 0-mkey 1-skey 2-either
+ */
+function parseKey(key: string, keyType: number): [string,string] {
+	let fields: string[];
+	if (keyType === 0) {
+		fields = mFields;
+	} else if (keyType === 1){
+		fields = sFields;
+	} else {
+		fields = mFields.concat(sFields);
+	}
+	let keys = key.split("_");
+	if (keys.length === 2) {
+		if (fields.includes(keys[1])) {
+			return [keys[0], keys[1]];
+		}
+	}
+	throw new Error("Syntax error: invalid key" + key);
+}
+
+// Interface for all filter classes
 interface Filter {
 	deserialize(input: any): void;
 	// TODO: each filter can filter an input from our dataset
 	// TODO: return a promise?
 	filterEntries(entries: unknown): unknown;
 }
-
 
 const logicOps = ["AND", "OR"];
 class LogicComparison implements Filter{
@@ -30,12 +146,12 @@ class LogicComparison implements Filter{
 		for (let op in input) {
 			if (logicOps.includes(op)){
 				this.logic = op;
-				let inputFilters = input.op;
+				let inputFilters = input[op];
 				// deserialize all filters
 				if (inputFilters instanceof Array) {
-					for (let inputFilter in inputFilters) {
+					for (let i in inputFilters) {
 						// puts all filter references in filters
-						this.filters.push(parseFilter(inputFilter));
+						this.filters.push(parseFilter(inputFilters[i]));
 					}
 					result = 1;
 				}
@@ -78,22 +194,14 @@ class MComparison implements Filter{
 			if (mComparators.includes(op)) {
 				this.mComparator = op;
 			}
-
 			// {' mkey ':' number '}
-			if (Object.keys(input.op).length > 1) {
+			if (Object.keys(input[op]).length > 1) {
 				return 0;
 			}
-			for (let mkey in input.op) {
-				let keys = mkey.split("_");
-				if (keys.length > 2) {
-					return 0;
-				}
-				if (mFields.includes(keys[1])) {
-					this.idString = keys[0];
-					this.mfield = keys[1];
-					this.num = input.op.mkey;
-					result = 1;
-				}
+			for (let mkey in input[op]) {
+				[this.idString, this.mfield] = parseKey(mkey, 0);
+				this.num = input[op][mkey];
+				result = 1;
 			}
 		}
 		return result;
@@ -108,7 +216,7 @@ const sFields = ["dept", "id", "instructor", "title", "uuid"];
 class SComparison implements Filter{
 	public idString: string;
 	public sfield: string;
-	public inputStr: string;
+	public inputStr: string; // TODO: check non-preceding *s
 
 	constructor() {
 		this.idString = ""; // id of the referenced dataset
@@ -130,20 +238,13 @@ class SComparison implements Filter{
 				return 0;
 			}
 			// {' skey ':' [*]? inputstring [*]? '}
-			if (Object.keys(input.op).length > 1) {
+			if (Object.keys(input[op]).length > 1) {
 				return 0;
 			}
-			for (let skey in input.op) {
-				let keys = skey.split("_");
-				if (keys.length > 2) {
-					return 0;
-				}
-				if (sFields.includes(keys[1])) {
-					this.idString = keys[0];
-					this.sfield = keys[1];
-					this.inputStr = input.op.skey;
-					result = 1;
-				}
+			for (let skey in input[op]) {
+				[this.idString, this.sfield] = parseKey(skey, 1);
+				this.inputStr = input[op][skey];
+				result = 1;
 			}
 		}
 		return result;
@@ -170,7 +271,7 @@ class Negation implements Filter{
 			if (op !== "NOT") {
 				return 0;
 			}
-			this.filter = parseFilter(input.op);
+			this.filter = parseFilter(input[op]);
 			result = 1;
 		}
 
@@ -182,35 +283,4 @@ class Negation implements Filter{
 	}
 }
 
-function parseFilter(input: any): Filter{
-	if (Object.keys(input).length === 1) {
-		for (let op in input) {
-			if (logicOps.includes(op)) {
-				let f = new LogicComparison();
-				if (f.deserialize(input) === 1) {
-					return f;
-				}
-			}
-			if (mComparators.includes(op)) {
-				let f = new MComparison();
-				if (f.deserialize(input) === 1) {
-					return f;
-				}
-			}
-			if (op === "IS") {
-				let f = new SComparison();
-				if (f.deserialize(input) === 1) {
-					return f;
-				}
-			}
-			if (op === "NOT") {
-				let f = new Negation();
-				if (f.deserialize(input) === 1) {
-					return f;
-				}
-			}
-		}
-	}
-
-	throw new Error("Parsing failed, syntax error");
-}
+export {parseQuery, Query, Filter, Options};
